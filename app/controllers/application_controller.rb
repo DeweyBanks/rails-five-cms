6d86553430b98f00bd2c2ac84be4a149c3921020
chrisfinne <chris.finne@gmail.com>
2018-01-02 22:47:05 -0800

    disable basic auth for test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: "foo", password: "bar", unless: ->{ Rails.env.development? }
+#  http_basic_authenticate_with name: "foo", password: "bar", unless: ->{ Rails.env.development? }
 end
