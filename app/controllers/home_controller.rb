74f05571271dbad241f0ddc05223ce6cbcae9cc2
deweybanks <deweybanks@gmail.com>
2018-01-22 12:45:20 -0500

    adds static pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def show
+    if valid_page?
+      # @physicians = Commander.all
+      # @coaches = Commander.all
+      render template: "home/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+
+    def valid_page?
+      File.exist?(Pathname.new(Rails.root + "app/views/home/#{params[:page]}.html.erb"))
+    end
+
 end
