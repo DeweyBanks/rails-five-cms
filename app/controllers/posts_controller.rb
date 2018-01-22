00b8081342e7c4ceba9c0d3e8b728cb344dec9e8
deweybanks <deweybanks@gmail.com>
2018-01-22 13:16:07 -0500

    adds posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.includes(:comments).order('created_at DESC')
+    respond_to do |f|
+      f.html {render :index }
+      f.json {render json: @posts}
+    end
+  end
+
+  def show
+    @post = Post.includes(:comments).find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,36 @@ Rails.application.routes.draw do
   resources :foos
 
   get "/pages/*page" => "home#show"
+  get "/admin", to: "admin/dashboard#index", module: "admin",  as: "admin"
+
+  namespace :admin do
+    resources :posts
+    resources :commanders
+    resources :users
+    resources :campaigns
+    resources :messages, only: [:index, :show]
+  end
+
+  resources :posts, only: [:index, :show] do
+    resources :comments
+  end
+  get "/posts/tags/:tag" => "posts#by_tag", as: "by_tag"
+
+  # Post.where.not("slug", nil).all.each do |post|
+  #   get "/#{post.slug}", controller: "posts", action: "show", id: post.id
+  # end
+
+  resources :comments do
+    resources :comments
+  end
+
+  resources :tags, only: [:index, :show]
+
+  resources :commanders, only: [:index, :show]
+  resources :messages, only: [:create, :new]
+
+  post "/email_list" => "home#email_list"
+
+  get "*path", to: redirect('/')
 
 end
