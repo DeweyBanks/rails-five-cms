68377526851fd4b2743d4dd1cb1f840ad3d84a48
deweybanks <deweybanks@gmail.com>
2018-01-23 13:15:53 -0500

    configures database for large key error, adds devise user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   http_basic_authenticate_with name: "foo", password: "bar", unless: ->{ Rails.env.development? }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122201044) do
+ActiveRecord::Schema.define(version: 20180123180426) do
 
   create_table "campaigns", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string "name"
@@ -88,7 +88,33 @@ ActiveRecord::Schema.define(version: 20180122201044) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "employer"
+    t.string "gender"
+    t.date "dob"
+    t.string "zipcode"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "role_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
+  end
+
   add_foreign_key "commanders", "commander_types"
   add_foreign_key "posts", "campaigns"
   add_foreign_key "posts", "categories"
+  add_foreign_key "users", "roles"
 end
