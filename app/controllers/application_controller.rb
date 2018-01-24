1bfcfb72e834c911f133258b3aa3ba1cc84cb264
deweybanks <deweybanks@gmail.com>
2018-01-24 12:43:54 -0500

    adds user sign up fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   http_basic_authenticate_with name: "foo", password: "bar", unless: ->{ Rails.env.development? }
 
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob, :gender, :employer, :zipcode])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,26 @@
 class User < ApplicationRecord
+  before_validation :set_role, on: :create
   belongs_to :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    if self.first_name.present? && self.last_name.present?
+      self.first_name + " " + self.last_name
+    else
+      self.email
+    end
+  end
+
+  private
+    def set_role
+      if self.role.present?
+        self.role = self.role
+      else
+        self.role = Role.find_by(name: "User")
+      end
+    end
+
 end
