dbd4e4c4aa2887187b5c911e806ce7a1aa7045be
deweybanks <deweybanks@gmail.com>
2018-02-02 16:28:06 -0500

    adds to comment test

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe CommentsController, type: :controller do
 
   describe "POST #create" do
 
-    context "with valid params" do
-
+    context "Comment on a Post" do
       before :each do
         @campaign = FactoryBot.create(:campaign, name: "Healthy Living")
         @category = Category.find_by(name: "News")
         @post = FactoryBot.create(:post, category: @category, campaign: @campaign)
       end
-
 
       let(:valid_params) {
         {'comment' => {
@@ -41,9 +39,45 @@ RSpec.describe CommentsController, type: :controller do
         post :create, params: valid_params
         expect(response).to redirect_to(post_path(@post))
       end
+    end
 
+    context "Comment on a Comment" do
+      before :each do
+        @campaign = FactoryBot.create(:campaign, name: "Healthy Living")
+        @category = Category.find_by(name: "News")
+        @post = FactoryBot.create(:post, category: @category, campaign: @campaign)
+        @comment = FactoryBot.create(:comment, commentable_type: "Post", commentable_id: @post.id)
+      end
+
+      let(:valid_params) {
+        {'comment' => {
+          'first_name' => 'Jane',
+          'last_name' => 'Doe',
+          'email' => 'jdoe@example.com',
+          'body' => 'Reply Test Comment'
+          },
+          'comment_id' => @comment.id
+        }
+      }
+
+      it 'creates a new Comment' do
+        expect {
+          post :create, params: valid_params
+        }.to change(Comment, :count).by(1)
+      end
+
+      it "assigns a newly created comment as @comment" do
+        post :create, params: valid_params
+        expect(assigns(:comment)).to be_a(Comment)
+        expect(assigns(:comment)).to be_persisted
+      end
+
+      it "redirects to the created comment" do
+        from post_path(@post)
+        post :create, params: valid_params
+        expect(response).to redirect_to(post_path(@post))
+      end
     end
 
   end
-
 end
