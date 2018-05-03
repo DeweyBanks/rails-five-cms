20af281676bdd8d9ea9fd8dce62703790109a4bc
Cam Crews <ccrews@eheintl.com>
2018-05-03 17:37:48 -0400

    Content updates requested by marketing

--- config/initializers/ehe.rb
+++ config/initializers/ehe.rb
@@ -1,6 +1,6 @@
 # EHE configuration settings
 EHEANDME_LANDING_URI = 'https://eheandme.com/landing/signin'.freeze
-EHEANDME_HOME = 'https://eheandme.com'.freeze
+EHEANDME_HOME = EHEANDME_LANDING_URI.freeze #'https://eheandme.com'.freeze
 
 # Internal Links
 EHE_COMMANDERS_IN_WELLNESS = '/commanders_in_wellness'.freeze
