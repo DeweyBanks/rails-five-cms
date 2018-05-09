b63416b6db64b67a7a881016d7b70b563b3d290a
Cam Crews <ccrews@eheintl.com>
2018-05-09 12:34:05 -0400

    Mentors -> Executive team for Commanders in Prevention, ehe initializer for meta description

--- config/initializers/ehe.rb
+++ config/initializers/ehe.rb
@@ -7,3 +7,5 @@ EHE_COMMANDERS_IN_WELLNESS = '/commanders_in_wellness'.freeze
 EHE_LOCATIONS = '/locations'.freeze
 EHE_ABOUT = '/about'.freeze
 EHE_TEAM = '/commanders_in_wellness'.freeze
+
+META_DESCRIPTION='For over 100 years, EHE has been the leader in health care and wellbeing. We are pioneers in employee health and lifestyle management.'.freeze
