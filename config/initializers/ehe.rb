5726faf5306476bb2e141635661006664899fe6f
Cam Crews <ccrews@eheintl.com>
2018-05-10 08:15:07 -0400

    Added GOOGLE_ANALYTICS initializer, added GOOGLE ANALYTICS to application.html.erb layout file

--- config/initializers/ehe.rb
+++ config/initializers/ehe.rb
@@ -8,4 +8,21 @@ EHE_LOCATIONS = '/locations'.freeze
 EHE_ABOUT = '/about'.freeze
 EHE_TEAM = '/commanders_in_wellness'.freeze
 
-META_DESCRIPTION='For over 100 years, EHE has been the leader in health care and wellbeing. We are pioneers in employee health and lifestyle management.'.freeze
+META_DESCRIPTION = 'For over 100 years, EHE has been the leader in health care and wellbeing. We are pioneers in employee health and lifestyle management.'.freeze
+
+GOOGLE_ANALYTICS =
+  if ENV['EHE_GOOGLE_ANALYTICS']
+    <<-TAG
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=#{ENV['EHE_GOOGLE_ANALYTICS']}"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '#{ENV['EHE_GOOGLE_ANALYTICS']}');
+      </script>
+    TAG
+  else
+    ''
+  end.freeze
